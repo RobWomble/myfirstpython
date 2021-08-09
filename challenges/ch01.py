8c964362e6059ab5eadddb22ab5c3f3ec7007e6b
Rob Womble <RobWomble@users.noreply.github.com>
2021-08-09 19:59:19 +0000

    added lab scripts and challenge 1

---
+++ challenges/ch01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+#NDE CHALLENGE #1 - Print
+
+#Set the following variables
+
+a = "little tough to create. Especially"
+b = "ones that are mixed up."
+c = "Sentences are sometimes a"
+
+#Use the print function to print the following.
+
+#Sentences are sometimes a @@@
+#little tough to create. Especially @@@
+#ones that are mixed up.
+
+print(c, a, b, sep=" @@@\n",end="")
+
+#Use ONLY the variables (a, b, c) as the *objects that get printed
+#Use both the sep= and end= to accomplish this task.
