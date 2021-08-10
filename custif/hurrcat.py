cd73e688d5a937848c7e62f2c053610ed12aee3a
Rob Womble <RobWomble@users.noreply.github.com>
2021-08-10 22:22:05 +0000

    updated hurricane script

--- custif/hurrcat.py
+++ custif/hurrcat.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+""" Alta3 Research | Rob Womble
+    A script to determine hurricane category based on wind speed in mph"""
 
+# Requesting user input for speed
 speed = float(input("Enter current wind speed in mph: "))
 
+# Using speed to determine category rating.
 if speed > 156:
     category = "Five"
 elif speed > 129:
@@ -14,6 +18,7 @@ elif speed > 73:
     category = "One"
 else:
     print("You are not currently experiencing a hurricane.  Congratulations!")
-    exit()
+    exit() #kill the script to avoid error when {category} isn't defined in final print statement 
 
+#output answer to user with previously defined category
 print(f"You are currently experiencing a Category {category} Hurricane. Seek immediate shelter.")
