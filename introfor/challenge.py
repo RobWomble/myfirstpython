65a56f45e1da035c9feb1b2edf82726dcdc2e0e0
Rob Womble <RobWomble@users.noreply.github.com>
2021-08-11 17:42:25 +0000

    commented lab challenge

--- introfor/challenge.py
+++ introfor/challenge.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+""" Alta3 Research | Rob Womble
+    lab 24 challenge: use for loops to display contents of a list of dictionaries """
 
 farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
          {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
          {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
 
-for farm in farms:
-    for animal in farm["agriculture"]:
-        print(f"{farm['name']} raises {animal}")
+for farm in farms:  #iterate each dictionary
+    for harvestable in farm["agriculture"]:  #iterate each farm item 
+        #display a line for each iteration of iteration, describing which items are at the farm
+        print(f"{farm['name']} raises {harvestable}")
