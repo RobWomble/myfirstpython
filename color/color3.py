9c8ce5e7cce9e1b23e1f960f19c95b63c4b28a5d
Rob Womble <RobWomble@users.noreply.github.com>
2021-08-11 22:53:56 +0000

    main functions and pip packages

---
+++ color/color3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Alta3 Research | Rob Womble
+    Testing crayons             """
+
+import crayons
+
+# I sure hope this is self-explanatory
+def main():
+    print(f"{crayons.blue('My name')} is {crayons.red('Rob Womble')}")
+
+main()
