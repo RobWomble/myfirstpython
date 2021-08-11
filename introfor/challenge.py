1a1c962dfe554a178e8a4b4427de077c47c5f0d0
Rob Womble <RobWomble@users.noreply.github.com>
2021-08-11 17:34:48 +0000

    for looping

---
+++ introfor/challenge.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+for farm in farms:
+    for animal in farm["agriculture"]:
+        print(f"{farm['name']} raises {animal}")
