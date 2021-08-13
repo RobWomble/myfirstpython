8f17010108b8ca2b675b7e50db445dcd973fcbcc
Rob Womble <RobWomble@users.noreply.github.com>
2021-08-13 16:02:02 +0000

    pylint

---
+++ smallscript.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Rob Womble
+
+Small script to print lines
+written to practice with pylint
+"""
+
+def main():
+    """main function: prints string"""
+    mynote = "print this string"
+    print(mynote)
+
+main()
